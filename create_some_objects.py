e0fda3cbb1ffa356e49b32f6081eb1b841d63ea0
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-11-17 08:37:41 +0500

    class object

--- create_some_objects.py
+++ create_some_objects.py
@@ -1,4 +1,7 @@
-from create_one_attribute import Person
+from create_one_attribute import Person as P
 
 #create an object named "p1" whose name is "Anvar"
 #create an object named "p2" whose name is "Shavkat"
+
+p1=P("Anvar")
+p2=P("Shavkat")
--- create_some_objects_to_list.py
+++ create_some_objects_to_list.py
@@ -1,7 +1,13 @@
-from create_one_attribute import Person
-
+from create_one_attribute import Person as P
 #Create an object named "p1" whose name is "Anvar"
 #Create an object named "p2" whose name is "Shavkat"
 #Create an object named "p3" whose name is "Jasur"
-
 #Add these objects to the "persons" named list
+
+p1=P("Anvar")
+p2=P("Shavkat")
+p3=P("Jasur")
+
+p=[p1.name,p2.name,p3.name]
+
+print(p)
